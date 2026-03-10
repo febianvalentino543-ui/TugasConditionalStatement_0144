d17ebddc989341f86e71509835d7534e35606d19
febianvalentino543-ui <febianvalentino543@gmail.com>
2026-03-10 22:38:28 +0700

    menambahkan fungsi menghitung BMI

--- BMI.cpp
+++ BMI.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 using namespace std;
 
+// Prosedur untuk memasukkan data
+void MasukkanData(float &berat, float &tinggi) {
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tinggi;
+}
+
+// Fungsi untuk menghitung BMI
+float MenghitungBMI(float berat, float tinggi) {
+    return berat / (tinggi * tinggi);
+}
+
